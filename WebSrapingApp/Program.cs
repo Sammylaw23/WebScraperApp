a84db14ebf0e7cda9f2cea518a02ea0fc97636d3
Sammylaw23 <lawalsamuel2310@gmail.com>
2021-05-28 03:10:50 +0100

    First Commit

---
+++ WebSrapingApp/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace WebSrapingApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+                string baseUrl = "";
+            try
+            {
+                //string baseUrl = "https://www.azlyrics.com/";
+                string url = "https://search.azlyrics.com/";
+                //string searchTerm = "Amy Grant";
+                //string searchSong = "A Mighty Fortress";
+                string searchTerm = "Mercy Chinwo";
+                string searchSong = "Excess Love";
+
+                var httpClient = new HttpClient();
+                var html = await httpClient.GetStringAsync(url);
+
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(html);
+
+
+                var btnMenus = doc.DocumentNode.Descendants("a")
+                .Where(node => node.GetAttributeValue("class", "").Equals("btn btn-menu")).ToList();
+
+                foreach (var menu in btnMenus)
+                {
+                    try
+                    {
+                        var menuPage = menu.GetAttributeValue("href", "");
+                        var httpClient2 = new HttpClient();
+                        var singlePage = await httpClient2.GetStringAsync("https:" + menuPage);
+                        doc.LoadHtml(singlePage);
+                        var tempList = doc.DocumentNode.Descendants("a").ToList();
+                        string searchTermLink = tempList.Where(x => x.OuterHtml.ToUpper()
+                        .Contains(searchTerm.ToUpper())).Select(x => x.GetAttributeValue("href", "")).FirstOrDefault();
+                        if (searchTermLink is null) continue;
+                        var foundPage = await httpClient.GetStringAsync(baseUrl + searchTermLink);
+                        doc.LoadHtml(foundPage);
+                        var songList = doc.DocumentNode.Descendants("div")
+                            .Where(node => node.GetAttributeValue("class", "")
+                            .Equals("listalbum-item")).ToList();
+                        if (songList.Count > 0)
+                        {
+                        }
+                        else continue;
+
+
+                        //string songUrl = songList.Where(x => x.InnerText.Contains(searchSong))
+                        //    .Select(x=>x.Descendants("a").ToList().Select(node => node.GetAttributeValue("href", ""))).FirstOrDefault();
+
+                        var songItem = songList.Where(x => x.InnerText.Contains(searchSong)).FirstOrDefault();
+                        if (songItem is null) continue;
+                        string songUrl = songItem.Descendants("a").FirstOrDefault().GetAttributeValue("href", "");
+
+                        var songPage = await httpClient.GetStringAsync(baseUrl + songUrl);
+                        doc.LoadHtml(songPage);
+                        var song = doc.DocumentNode.Descendants("div")
+                            .Where(node => node.GetAttributeValue("class", "").Equals("lyricsh")).FirstOrDefault();
+
+
+
+                        //var song = doc.DocumentNode.Descendants("div")
+                        //    .Where(node => node.GetAttributeValue("class", "").Equals(" ")).FirstOrDefault().InnerHtml;
+                    }
+                    catch (Exception ex)
+                    {
+
+                        Console.WriteLine(ex.Message);
+                        Console.WriteLine(ex.StackTrace);
+                    }
+                    
+
+                }
+            }
+            catch (Exception ex) when (baseUrl == string.Empty)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+        }
+    }
+}
---
+++ WebSrapingApp/ProgramOld.cs
@@ -0,0 +1,228 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//using CsvHelper;
+//using HtmlAgilityPack;
+//using System.IO;
+//using System.Globalization;
+//using ScrapySharp.Network;
+//using ScrapySharp.Extensions;
+//using ScrapySharp.Html;
+//using ScrapySharp.Html.Forms;
+
+//namespace WebSrapingApp
+//{
+//    class ProgramOld
+//    {
+//        static ScrapingBrowser _browser = new ScrapingBrowser();
+
+//        static void Main(string[] args)
+//        {
+//            try
+//            {
+//                Console.WriteLine("Please enter a search term:");
+//                var MySearchTerm = "Amy Grant";
+//                //var MySearchTerm = Console.ReadLine();
+//                //https://search.azlyrics.com/search.php?q=mercy+chinwo
+//                var mainPageLinks = GetMainPageLinks("https://search.azlyrics.com");
+//                var lstGigs = GetPageDetails(mainPageLinks, MySearchTerm);
+//            }
+//            catch (Exception ex)
+//            {
+
+//                Console.WriteLine(ex.Message);
+//                Console.WriteLine(ex.StackTrace);
+//            }
+
+
+//            /*static*/
+//            List<string> GetMainPageLinks(string url)
+//            {
+//                var homePageLinks = new List<string>();
+
+//                try
+//                {
+//                    var html = GetHtml(url);
+//                    var links = html.CssSelect("a");
+
+//                    foreach (var link in links)
+//                    {
+//                        if (link.Attributes["href"].Value.Contains(".html"))
+//                        {
+//                            homePageLinks.Add(link.Attributes["href"].Value);
+//                        }
+//                    }
+//                }
+//                catch (Exception ex)
+//                {
+
+//                    Console.WriteLine(ex.Message);
+//                    Console.WriteLine(ex.StackTrace);
+//                }
+
+//                return homePageLinks;
+//            }
+
+
+//            /*static*/
+//            HtmlNode GetHtml(string url)
+//            {
+//                try
+//                {
+//                    WebPage webpage = _browser.NavigateToPage(new Uri(url));
+//                    return webpage.Html;
+//                }
+//                catch (Exception ex)
+//                {
+
+//                    Console.WriteLine(ex.Message);
+//                    Console.WriteLine(ex.StackTrace);
+//                }
+
+//                return null;
+//            }
+
+//            HtmlNode GetHtmlLinks(string url)
+//            {
+//                try
+//                {
+//                    //WebPage webpage = _browser.NavigateToPage(new Uri(url));
+//                    //return webpage.Html;
+//                    var doc = new HtmlDocument().LoadHtml(html: url);
+//                    var divs = doc.DocumentNode.Descendants("div");
+//                    var temp = divs.Select(x => x.GetDataAttributes());
+
+
+//                    //IEnumerable<HyperLink> links = FindLinks(By by);
+//                }
+//                catch (Exception ex)
+//                {
+
+//                    Console.WriteLine(ex.Message);
+//                    Console.WriteLine(ex.StackTrace);
+//                }
+
+//                return null;
+//            }
+
+
+
+
+//            /*static*/
+//            List<PageDetails> GetPageDetails(List<string> urls, string searchTerm)
+//            {
+//                var lstPageDetails = new List<PageDetails>();
+//                try
+//                {
+//                    foreach (var url in urls)
+//                    {
+//                        //var htmlNode = GetHtml(url);
+//                        var htmlNode = GetHtmlLinks(url);
+//                        var pageDetails = new PageDetails()
+//                        {
+//                            Title = htmlNode.OwnerDocument.DocumentNode.SelectSingleNode("//html/head/title").InnerText,
+//                            Description = htmlNode.OwnerDocument.DocumentNode
+//                          .SelectSingleNode("//html/body/section/section/section/section").InnerText
+//                          .Replace("\n        \n            QR Code Link to This Post\n            \n        \n", ""),
+//                            Url = url
+//                        };
+
+
+//                        //var pageDetails = new PageDetails();
+//                        //pageDetails.Title = htmlNode.OwnerDocument.DocumentNode
+//                        //  .SelectSingleNode("//html/head/title").InnerText;
+
+//                        //var description = htmlNode.OwnerDocument.DocumentNode
+//                        //  .SelectSingleNode("//html/body/section/section/section/section").InnerText;
+//                        //pageDetails.Description = description
+//                        //  .Replace("\n        \n            QR Code Link to This Post\n            \n        \n", "");
+
+//                        //pageDetails.Url = url;
+
+//                        var searchTermInTitle = pageDetails.Title.ToLower().Contains(searchTerm.ToLower());
+//                        var searchTermInDescription = pageDetails.Description.ToLower().Contains(searchTerm.ToLower());
+
+//                        if (searchTermInTitle || searchTermInDescription)
+//                        {
+//                            lstPageDetails.Add(pageDetails);
+//                        }
+//                    }
+//                }
+//                catch (Exception ex)
+//                {
+
+//                    Console.WriteLine(ex.Message);
+//                    Console.WriteLine(ex.StackTrace);
+//                }
+
+
+//                return lstPageDetails;
+//            }
+
+
+
+//        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        //static void Main(string[] args)
+//        //{
+//        //    HtmlWeb web = new HtmlWeb();
+//        //    HtmlDocument doc = web.Load("https://search.azlyrics.com/search.php?q=mercy+chinwo");
+//        //    var panels = doc.DocumentNode.SelectNodes("//div[@class='panel-heading");
+
+//        //    foreach(var item in panels)
+//        //    {
+//        //        var HeaderNames = doc.DocumentNode.SelectNodes("//table[@class='table table-condensed']");
+//        //        HeaderNames = HeaderNames.d
+//        //    }
+
+//        //    //var HeaderNames = doc.DocumentNode.SelectNodes("//span[@class='toctext']");
+
+
+
+//        //    var titles = new List<Row>();
+//        //    foreach (var item in HeaderNames)
+//        //    {
+//        //        titles.Add(new Row { Title = item.InnerText });
+//        //    }
+
+//        //    var folder = AppDomain.CurrentDomain.BaseDirectory + "/files";
+//        //    if (!Directory.Exists(folder))
+//        //        Directory.CreateDirectory(folder);
+
+//        //    using (var writer = new StreamWriter($"{folder}/example.csv"))
+//        //    using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+//        //    {
+//        //        csv.WriteRecords(titles);
+//        //    }
+//        //}
+//    }
+
+//    public class PageDetails
+//    {
+//        public string Title { get; set; }
+//        public string Description { get; set; }
+//        public string Url { get; set; }
+//    }
+//}
